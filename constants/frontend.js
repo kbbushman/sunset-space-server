2aaf827ae428acfe3de050f8229e54fd933788b6
kbbushman <k.bushman@yahoo.com>
2018-12-04 13:57:25 -0800

    updates

--- constants/frontend.js
+++ constants/frontend.js
@@ -2,6 +2,7 @@ const FRONTEND_DEV_URLS = [ 'http://localhost:3000' ];
 
 const FRONTEND_PROD_URLS = [ 
     'https://ss-test-client.herokuapp.com',
+    'https://ss-test-server.herokuapp.com/',
     'mongodb://heroku_pgglj6qc:tqinmqvr9lgresl73c3d1tk5kq@ds119210.mlab.com:19210/heroku_pgglj6qc'
  ];
 
