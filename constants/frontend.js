9e1c7502e6a3ce6c1fcb95c0cb5fd57f0d200c13
atfcreative <atfcreative@gmail.com>
2018-12-04 11:00:58 -0800

    Updates stripe config

--- constants/frontend.js
+++ constants/frontend.js
@@ -5,7 +5,7 @@ const FRONTEND_PROD_URLS = [
     'mongodb://heroku_pgglj6qc:tqinmqvr9lgresl73c3d1tk5kq@ds119210.mlab.com:19210/heroku_pgglj6qc'
  ];
 
- module.exports = process.env.NODE_ENV === FRONTEND_PROD_URLS;
+ module.exports = FRONTEND_PROD_URLS;
 
 //  module.exports = process.env.NODE_ENV === 'production'
 //  ? FRONTEND_PROD_URLS
